f87297babf6766a18458bb9ea643e0e6615bacb0
John Devitt <johndevittire@gmail.com>
2014-12-05 20:06:46 +0000

    fixes to pub model

--- app/models/pub.rb
+++ app/models/pub.rb
@@ -2,4 +2,17 @@ class Pub < ActiveRecord::Base
 
 	has_and_belongs_to_many :routes
 
+	geocoded_by :full_address
+	after_validation :geocode
+
+	attr_accessor :streetName
+
+	def full_address
+		unless streetName.nil?
+			name + " ," + streetName + ",Dublin,Ireland"
+		end
+
+		name + " ,Dublin,Ireland"
+	end
+
 end
